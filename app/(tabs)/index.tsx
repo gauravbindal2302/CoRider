fbcad221751f8606a73905a3e5f2c84e4c7d87e2
Gaurav Bindal <gb2302bindal@gmail.com>
2025-05-08 14:51:03 +0530

    Code for Toggle Menu and Profile Image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import {
   FlatList,
+  Image,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -19,6 +20,11 @@ export default function ChatScreen() {
     to: ""
   })
 
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(prevState => !prevState);
+  };
+
   const api = "https://qa.corider.in/assignment/chat?page=0"
 
   useEffect(() => {
@@ -27,8 +33,9 @@ export default function ChatScreen() {
         const res = await axios.get(`${api}`);
         const data = res.data.chats.map((chat) => ({
           id: chat.id,
-          text: chat.message.replace(/<br>/g, '\n'),
+          message: chat.message.replace(/<br>/g, '\n'),
           sender: chat.sender.self ? 'me' : 'other',
+          image: chat.sender.image,
           time: chat.time,
         }));
         setTripInfo({
@@ -50,15 +57,24 @@ export default function ChatScreen() {
   
     return (
       <View>
-         <Text style={styles.timeText}>{dateTime}</Text>
-        <View style={[styles.messageContainer, isMe ? styles.myMessage : styles.otherMessage]}>
-          <Text style={[styles.messageText, isMe ? styles.myText : styles.otherText]}>
-            {item.text}
-          </Text>
+        <Text style={styles.timeText}>{dateTime}</Text>
+        <View style={[styles.row, isMe ? styles.rightAlign : styles.leftAlign]}>
+          {!isMe && (
+            <Image
+              source={{ uri: item.image }}
+              style={styles.profileImage}
+            />
+          )}
+          <View style={[styles.messageContainer, isMe ? styles.myMessage : styles.otherMessage]}>
+            <Text style={[styles.messageText, isMe ? styles.myText : styles.otherText]}>
+              {item.message}
+            </Text>
+          </View>
         </View>
       </View>
     );
-  };
+  };  
+  
   
   return (
     <SafeAreaView style={styles.container}>
@@ -74,10 +90,27 @@ export default function ChatScreen() {
             From <Text style={styles.bold}>{tripInfo.from}</Text> To <Text style={styles.bold}>{tripInfo.to}</Text>
           </Text>
         </View>
-        <TouchableOpacity style={styles.menuIcon}>
+        <TouchableOpacity style={styles.menuIcon} onPress={toggleMenu}>
           <Entypo name="dots-three-vertical" size={18} color="black" />
         </TouchableOpacity>
       </View>
+
+      {showMenu && (
+        <View style={styles.menu}>
+          <TouchableOpacity style={styles.menuItem}>
+            <Ionicons name="person-outline" size={20} color="black" style={styles.menuIconLeft} />
+            <Text style={styles.menuText}>Members</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Ionicons name="call-outline" size={20} color="black" style={styles.menuIconLeft} />
+            <Text style={styles.menuText}>Share Number</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem}>
+            <Ionicons name="chatbubble-ellipses-outline" size={20} color="black" style={styles.menuIconLeft} />
+            <Text style={styles.menuText}>Report</Text>
+          </TouchableOpacity>
+        </View>
+      )}
 
       {/* Messages Container */}
       <FlatList
@@ -122,6 +155,35 @@ const styles = StyleSheet.create({
   subText: { fontSize: 14, color: '#444' },
   bold: { fontWeight: 'bold' },
   menuIcon: { marginLeft: 'auto' },
+  menu: {
+    position: 'absolute',
+    top: 85,
+    right: 16,
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    paddingVertical: 6,
+    width: 180,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+    zIndex: 100,
+  },
+  menuItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderBottomColor: '#eee',
+  },
+  menuIconLeft: {
+    marginRight: 12,
+  },
+  menuText: {
+    fontSize: 15,
+    color: '#111',
+  },
   dateText: {
     alignSelf: 'center',
     fontSize: 12,
@@ -137,7 +199,7 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 12,
     marginVertical: 6,
-    maxWidth: '85%',
+    maxWidth: '75%',
   },
   myMessage: {
     backgroundColor: '#007AFF',
@@ -146,7 +208,26 @@ const styles = StyleSheet.create({
   otherMessage: {
     backgroundColor: '#F1F1F1',
     alignSelf: 'flex-start',
+    display: 'flex',
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  profileImage: {
+    width: 24,
+    height: 24,
+    borderRadius: 18,
+    marginRight: 8,
+    marginVertical: 6,
+  },
+  leftAlign: {
+    alignSelf: 'flex-start',
+  },
+  rightAlign: {
+    alignSelf: 'flex-end',
+    flexDirection: 'row-reverse',
+  },  
   messageText: {
     fontSize: 14,
   },
